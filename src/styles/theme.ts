ac9e5cbc43878cb19394197c395ad1e64877b7c4
T14700030 <raphael.brito@opah.com.br>
2020-10-21 01:47:20 -0300

    first commit

---
+++ src/styles/theme.ts
@@ -0,0 +1,12 @@
+const theme = {
+  colors: {
+    background: 'linear-gradient(180deg, #0D0814 0%, #13111D 100%)',
+    bg1: '#0D0814',
+    bg2: '#13111D',
+    text: '#DEDEF5',
+    primary: '#F5D575',
+    secondary: '#768CF5'
+  }
+}
+
+export default theme
